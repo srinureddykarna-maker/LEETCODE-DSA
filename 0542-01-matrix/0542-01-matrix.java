88b8486ae867b0b0e80a1660a1cad6247250bbb3
KARNA SRINIVASULU REDDY <srinureddykarna@gmail.com>
2026-09-16 14:56:02 +0530

    Time: 16 ms (28.13%), Space: 49.1 MB (52.06%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,64 +1,46 @@
-class Pair {
-    int r;
-    int c;
-    int d;
-
-    Pair(int r, int c, int d) {
-        this.r = r;
-        this.c = c;
-        this.d = d;
-    }
-}
-
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
 
         int n = mat.length;
         int m = mat[0].length;
 
-        int[][] visited = new int[n][m];
         int[][] ans = new int[n][m];
+        Queue<int[]> q = new ArrayDeque<>();
 
-        Queue<Pair> q = new LinkedList<>();
-
-        // Put all 0s into the queue
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
 
                 if (mat[i][j] == 0) {
-                    q.add(new Pair(i, j, 0));
-                    visited[i][j] = 1;
+                    ans[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                } else {
+                    ans[i][j] = -1;
                 }
             }
         }
 
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, -1, 0, 1};
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, -1, 0, 1};
 
-        // BFS
         while (!q.isEmpty()) {
 
-            Pair p = q.poll();
+            int[] curr = q.poll();
 
-            int r = p.r;
-            int c = p.c;
-            int d = p.d;
+            int r = curr[0];
+            int c = curr[1];
 
-            ans[r][c] = d;
-
-            // Check 4 directions
             for (int i = 0; i < 4; i++) {
 
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
                 if (nr >= 0 && nr < n &&
                     nc >= 0 && nc < m &&
-                    visited[nr][nc] == 0) {
+                    ans[nr][nc] == -1) {
 
-                    visited[nr][nc] = 1;
+                    ans[nr][nc] = ans[r][c] + 1;
 
-                    q.add(new Pair(nr, nc, d + 1));
+                    q.offer(new int[]{nr, nc});
                 }
             }
         }
