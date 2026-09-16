32ba132840120f64e4238135a0088cd3ee66051c
KARNA SRINIVASULU REDDY <srinureddykarna@gmail.com>
2026-09-16 13:45:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,54 +1,73 @@
-class pair {
+class Pair {
     int node;
     int parent;
 
-    pair(int node, int parent) {
+    Pair(int node, int parent) {
         this.node = node;
         this.parent = parent;
     }
 }
 
 class Solution {
+
     public boolean isCycle(int V, int[][] edges) {
-        // Code here
+
         // 1. Create adjacency list
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-            for (int i = 0; i < V; i++) {
-                adj.add(new ArrayList<>());
-            }
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
 
-            for (int[] edge : edges) {
-                int u = edge[0];
-                int v = edge[1];
+        // 2. Add edges
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
 
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
 
-            boolean[] visited = new boolean[V];
-            
-            for(int i=0;i<V;i++){
-                if(!visited[i]){
-                    Queue<pair> q = new LinkedList<>();
-                    q.offer(new pair(i,-1));
-                    visited[i]=true;
-                    while(!q.isEmpty()){
-                        pair current = q.poll();
-                        int node = current.node;
-                        int parent = current.parent;
-                        for(int neighbour : adj.get(node)){
-                            if(!visited[neighbour]){
-                                visited[neighbour] =true;
-                                q.offer(new pair(neighbour,node));
-                                
-                            }else if( neighbour != parent){
-                                return true;
-                            }
-                        }
-                    }
+        // 3. Visited array
+        boolean[] visited = new boolean[V];
+
+        // 4. Check every component
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, -1, adj, visited)) {
+                    return true;
                 }
             }
-            return false;
+        }
+
+        return false;
+    }
+
+    private boolean dfs(
+        int node,
+        int parent,
+        ArrayList<ArrayList<Integer>> adj,
+        boolean[] visited
+    ) {
+
+        visited[node] = true;
+
+        for (int neighbour : adj.get(node)) {
+
+            // If neighbour is not visited
+            if (!visited[neighbour]) {
+
+                if (dfs(neighbour, node, adj, visited)) {
+                    return true;
+                }
+
+            }
+            // Neighbour already visited and it is NOT parent
+            else if (neighbour != parent) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
