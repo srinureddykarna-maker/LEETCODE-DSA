15a0022232ae5dfc2f106fb619179a4baa1b6871
KARNA SRINIVASULU REDDY <srinureddykarna@gmail.com>
2026-09-18 18:01:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.java
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.java
@@ -1,30 +1,31 @@
 class Solution {
 
-    private boolean check(ArrayList<ArrayList<Integer>> adj, int start, int[] clr) {
+    private boolean checkdfs(ArrayList<ArrayList<Integer>> adj, int start, int color,int[] clr) {
 
-        Queue<Integer> q = new LinkedList<>();
+        //Queue<Integer> q = new LinkedList<>();
 
-        q.add(start);
-        clr[start] = 0;
+       // q.add(start);
+        clr[start] = color;
 
-        while (!q.isEmpty()) {
+       // while (!q.isEmpty()) {
 
-            int node = q.peek();
-            q.remove();
+           // int node = q.peek();
+            //q.remove();
 
-            for (int i = 0; i < adj.get(node).size(); i++) {
+            for (int i = 0; i < adj.get(start).size(); i++) {
 
-                int neighbor = adj.get(node).get(i);
+                int neighbour = adj.get(start).get(i);
 
-                if (clr[neighbor] == -1) {
-                    clr[neighbor] = 1 - clr[node];
-                    q.add(neighbor);
+                if (clr[neighbour] == -1) {
+                   if(checkdfs(adj,neighbour,1-color,clr)==false){
+                       return false;
+                   }
                 }
-                else if (clr[neighbor] == clr[node]) {
+                else if (clr[neighbour] == clr[start]) {
                     return false;
                 }
             }
-        }
+        
 
         return true;
     }
@@ -58,7 +59,7 @@ class Solution {
 
             if (clr[i] == -1) {
 
-                if (check(adj, i, clr) == false) {
+                if (checkdfs(adj, i,0, clr) == false) {
                     return false;
                 }
             }
