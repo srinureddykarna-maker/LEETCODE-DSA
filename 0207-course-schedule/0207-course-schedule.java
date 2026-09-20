f6c8ba2e843a4fbe979c22287b55780cf116511c
KARNA SRINIVASULU REDDY <srinureddykarna@gmail.com>
2026-09-20 18:27:03 +0530

    Time: 7 ms (56.64%), Space: 47 MB (77.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,48 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int V = numCourses;
-        for(int i=0;i<V;i++){
+
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-        int[] indegree = new int[V];
 
-              for (int[] edge : prerequisites) {
-                  int u = edge[0];
-                  int v = edge[1];
+        int[] indegree = new int[numCourses];
 
-                  adj.get(v).add(u);
-                  indegree[u]++;
-              }
-              Queue<Integer> q = new LinkedList<>();
-              for(int i=0;i<V;i++){
-                  if(indegree[i]==0){
-                      q.add(i);
-                      
-                  }
-              }
-              int count=0;
-              while(!q.isEmpty()){
-                int node = q.poll();
-                count++;
-                for(int it:adj.get(node)){
-                    indegree[it]--;
-                    if(indegree[it]==0){
-                        q.add(it);
-                    }
+        // Build graph
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prerequisite = edge[1];
+
+            adj.get(prerequisite).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        // Courses with no prerequisites
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+
+            int node = q.poll();
+            count++;
+
+            for (int next : adj.get(node)) {
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    q.add(next);
                 }
-              }
-        if(V!=count){
-            return false;
-        }else return true;
+            }
+        }
+
+        return count == numCourses;
     }
 }
